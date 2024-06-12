d776c15564bcde611ed5796e789f8fc1cd81eae2
Joel Tay <joeltwh1999@gmail.com>
2024-06-12 19:45:22 +0800

    fix carousel clipping

--- src/components/InfiniteCarousel.js
+++ src/components/InfiniteCarousel.js
@@ -5,29 +5,29 @@ export default function InfiniteCarousel({ imageUrl, className }) {
     <div className={`relative w-screen h-full ${className} overflow-hidden`}>
       <motion.div
         className="flex h-full absolute top-0 left-0"
-        initial={{ x: 0 }}
-        animate={{ x: '-200vw' }}
+        initial={{ x: '110vw' }}
+        animate={{ x: '-310vw' }}
         transition={{
           repeat: Infinity,
           repeatType: 'loop',
-          duration: 20, // Adjust the duration to control the speed
+          duration: 20,
           ease: 'linear',
         }}
       >
-        <div className="w-[200vw] h-full flex-shrink-0">
+        <div className="h-full flex-shrink-0">
+          <img
+            src={imageUrl}
+            className="h-full object-cover"
+            alt="carousel"
+          />
+        </div>
+        {/* <div className="w-[200vw] h-full flex-shrink-0">
           <img
             src={imageUrl}
             className="w-full h-full object-cover"
             alt="carousel"
           />
-        </div>
-        <div className="w-[200vw] h-full flex-shrink-0">
-          <img
-            src={imageUrl}
-            className="w-full h-full object-cover"
-            alt="carousel"
-          />
-        </div>
+        </div> */}
       </motion.div>
     </div>
   )
