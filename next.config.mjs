cdb6b8264602337f1da1e870717b737b80f00aba
Joel Tay <joeltwh1999@gmail.com>
2024-06-28 15:36:52 +0800

    attempt

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 import mdx from '@next/mdx'
 
-const withMDX = mdx()
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'mdx']
 };
 
-export default withMDX(nextConfig)
+export default mdx()(nextConfig)
