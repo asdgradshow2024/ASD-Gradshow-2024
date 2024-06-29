b2f3fbcd4125e794608dc3fa2ff7a5a73680a1c7
Joel Tay <joeltwh1999@gmail.com>
2024-06-29 14:20:45 +0800

    attempt

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ import mdx from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'mdx']
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  typescript: {
+    ignoreBuildErrors: true
+  }
 };
 
 const withMdx = mdx();
