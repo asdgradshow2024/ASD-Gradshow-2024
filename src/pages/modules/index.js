17f142ddda1d160df0f0fb841f4b5b96af3afd89
Kong Mei Jia <kongmeijiaa@gmail.com>
2024-07-13 01:19:22 +0800

    modules images

--- src/pages/modules/index.js
+++ src/pages/modules/index.js
@@ -11,19 +11,19 @@ const ModulesPage = ({
 }) => {
   return (
     <div className="py-4 md:px-12">
-      <Image src={projectsHeader} alt="projectsHeader" priority />
-      <div className="grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] md:grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-6 mt-6">
+      <Image src={projectsHeader} alt="projectsHeader" priority className="scale-[0.85]" />
+      <div className="grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] md:grid-cols-[repeat(auto-fit,_minmax(230px,_1fr))] gap-6 mt-6">
         {
           modules.map((module, i) => (
             <div key={i}>
               <Link href={module?.moduleUrl || '/'}>
-                <div className="w-full h-[250px] md:h-[300px] relative">
+                <div className="w-full h-[180px] md:h-[230px] relative">
                   <Image src={`${prefix}/${module.thumbnailPath}`} alt={module.name} fill style={{ objectFit: 'contain' }}/>
                 </div>
               </Link>
-              <div className="text-center">
-                <p className={`${ebGaramond.className} text-3xl font-bold`}>{module.name}</p>
-                <p className={`${nunito.className} text-xl italic font-semibold`}>{module.timeframe}</p>
+              <div className="text-center mt-4">
+                <p className={`${ebGaramond.className} text-2xl font-bold`}>{module.name}</p>
+                <p className={`${nunito.className} text-lg italic font-semibold`}>{module.timeframe}</p>
               </div>
             </div>
           ))
