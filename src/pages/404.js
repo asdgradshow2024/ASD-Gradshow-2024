c3a188c8d662ecd2ed76b42c3935341052d01e02
Joel Tay <joeltwh1999@gmail.com>
2024-07-18 01:32:53 +0800

    404 image

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,11 @@
 import Image from "next/image"
-import characterBanner from '../../public/characters.png'
+import Error404 from '../../public/404-error.png'
 
 export default function Custom404() {
   return (
-    <div className="p-8">
+    <div className="p-8 md:w-1/2 md:m-auto">
       <Image
-        src={characterBanner}
+        src={Error404}
         alt="404 banner"
         priority
       />
